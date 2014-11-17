682c3d0f0838245726f93268a150156cd8e1cf4e
Chris Lawrence <c@chrislawrence.co>
2014-11-17 14:07:13 +1100

    send to Mailchimp when updating introduction

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -5,7 +5,9 @@ class EmailProcessor
 
   def process
     if Lakecinema::Application.config.admin_emails.include?(@email.from[:email])
-      Newsletter.first.update_attribute(:introduction, @email.body)
+      newsletter = Newsletter.first
+      newsletter.update_attribute(:introduction, @email.body)
+      newsletter.send_to_mailchimp
     end
   end
 end
